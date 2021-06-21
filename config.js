c7f85fb9ef1df0a624aaa69caef7f9c66955e85f
215165488949 <86228841+215165488949@users.noreply.github.com>
2021-06-21 16:16:17 +0700

    Create config.js

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 const CONFIG = {
     introTitle: 'cậu à!',
     introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
+    Mà sao cậu cứ đi nhầm đường
+    Lạc vào tim tớ lẻ loi
     Đằng sau chữ yêu đây là thương`,
     btnIntro: 'hihi',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333',
+    mess: 'tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
+    messDesc: 'yêu cậu lắm đó 😘😘🥰 .',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
