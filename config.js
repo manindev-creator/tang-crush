67577ef53936613f218e83761d7202200476ed34
215165488949 <86228841+215165488949@users.noreply.github.com>
2021-06-21 16:17:46 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333333333333333333333333333333333333333333333333333333333333333333333333333333333333333',
     mess: 'tớ biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'yêu cậu lắm đó 😘😘🥰 .',
     btnAccept: 'Okiiiii lun <3',
